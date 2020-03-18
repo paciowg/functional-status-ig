8bde7798bd957072529ec0d654e4c05ed646c64d
timster <timster@users.noreply.github.com>
2020-03-18 14:04:55 +0000

    Automated deployment: Wed Mar 18 14:04:55 UTC 2020 686b30ae018459d91fb5c543a68cd5e517832f1e

--- HandlingRisk.java
+++
@@ -1,7 +0,0 @@
-package http://hl7.org/fhir/us/PACIO-functional-cognitive-status/ImplementationGuide/hl7.fhir.us.PACIO-functional-status-0.1.0;
-
-import org.hl7.fhir.r5.model.ProfilingWrapper;
-
-public class HandlingRisk {
-
-}
